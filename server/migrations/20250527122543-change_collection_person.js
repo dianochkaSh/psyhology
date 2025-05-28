bcb0cf0dccc7a8c874e016e66cbb79dd65621494
d.shevchenko <dianadanilenko@gmail.com>
2025-05-28 14:20:22 +0300

    migartion add new three column to collection people

---
+++ server/migrations/20250527122543-change_collection_person.js
@@ -0,0 +1,36 @@
+module.exports = {
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async up(db, client) {
+    await db.collection('people').updateMany({},
+      { $set:
+          {
+            phone: "+79286111360",
+            time_consultation : "90 минут",
+            format_consultation : "Очно и онлайн"
+          }
+
+
+      });
+
+    //
+    //   // .insertOne({"phone": "+79286111360" });
+    // await db.collection('people').insertOne({"time_consultation" : "90 минут"});
+    // await db.collection('people').insertOne({"format_consultation" : "Очно и онлайн "});
+
+  },
+
+  /**
+   * @param db {import('mongodb').Db}
+   * @param client {import('mongodb').MongoClient}
+   * @returns {Promise<void>}
+   */
+  async down(db, client) {
+    await db.collection('people').deleteOne({"phone": "+79286111360" });
+    await db.collection('people').deleteOne({"time_consultation" : "90 минут"});
+    await db.collection('people').deleteOne({"format_consultation" : "Очно и онлайн "});
+  }
+};
